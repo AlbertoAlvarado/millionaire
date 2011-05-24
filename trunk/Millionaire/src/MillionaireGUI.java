fdae1ddd4a172445200c70aa453f2d316a7c5846
gbronsdon@gmail.com <gbronsdon@gmail.com@6b946793-2454-46a0-8f8c-39d074f2bf81>
2011-05-24 18:00:37 +0000

    Added walk away button.

--- trunk/Millionaire/src/MillionaireGUI.java
+++ trunk/Millionaire/src/MillionaireGUI.java
@@ -11,52 +11,142 @@ public class MillionaireGUI {
 	private JPanel answerB = new JPanel();
 	private JPanel answerC = new JPanel();
 	private JPanel answerD = new JPanel();
+	private JPanel walkAway = new JPanel();
 	
 	private JTextField questionField;
 	private JButton answerAButton;
 	private JButton answerBButton;
 	private JButton answerCButton;
 	private JButton answerDButton;
+	private JButton walkAwayButton;
 	
-	public MillionaireGUI (Question q) {
-		questionField = new JTextField(q.getQuestion());
+	private GameManager gm;
+	
+	public MillionaireGUI (GameManager gm) {
+		this.gm = gm;
+		
+		questionField = new JTextField(gm.getQ().getQuestion());
 		questionField.setEditable(false);
-		answerAButton = new JButton(q.getAnswerA());
-		answerBButton = new JButton(q.getAnswerB());
-		answerCButton = new JButton(q.getAnswerC());
-		answerDButton = new JButton(q.getAnswerD());
+		answerAButton = new JButton(gm.getQ().getAnswerA());
+		answerBButton = new JButton(gm.getQ().getAnswerB());
+		answerCButton = new JButton(gm.getQ().getAnswerC());
+		answerDButton = new JButton(gm.getQ().getAnswerD());
+		walkAwayButton = new JButton("Walk Away");
 		
 		question.add(questionField);
 		answerA.add(answerAButton);
-		ActionListener listener = new MyActionListener();
-		answerAButton.addActionListener(listener);
+		ActionListener listenerA = new AActionListener();
+		answerAButton.addActionListener(listenerA);
+		ActionListener listenerB = new BActionListener();
+		answerBButton.addActionListener(listenerB);
 		answerB.add(answerBButton);
+		ActionListener listenerC = new CActionListener();
+		answerCButton.addActionListener(listenerC);
 		answerC.add(answerCButton);
+		ActionListener listenerD = new DActionListener();
+		answerDButton.addActionListener(listenerD);
 		answerD.add(answerDButton);
+		ActionListener walkListener = new WalkListener();
+		walkAwayButton.addActionListener(walkListener);
+		walkAway.add(walkAwayButton);
 		
 		f.getContentPane().add(question);
 		f.getContentPane().setLayout(new GridBagLayout());
 		f.getContentPane().add(answerA);
 		f.getContentPane().add(answerB);
 		f.getContentPane().add(answerC);
-		f.getContentPane().add(answerD);	
+		f.getContentPane().add(answerD);
+		f.getContentPane().add(walkAway);
 	}
 	
-	public class MyActionListener implements ActionListener {
-		
+	public class AActionListener implements ActionListener {	
 		public void actionPerformed(ActionEvent event) {
-			System.out.println("HELLO WORLD");
+			if (gm.getQ().getCorrectAnswer().equals("A")) {
+				updateCorrect();
+			}
+			else {
+				updateIncorrect();
+			}
 		}	
-		
 	}
+	
+	public class BActionListener implements ActionListener {	
+		public void actionPerformed(ActionEvent event) {
+			if (gm.getQ().getCorrectAnswer().equals("B")) {
+				updateCorrect();
+			}
+			else {
+				updateIncorrect();
+			}
+		}	
+	}
+	
+	public class CActionListener implements ActionListener {	
+		public void actionPerformed(ActionEvent event) {
+			if (gm.getQ().getCorrectAnswer().equals("C")) {
+				updateCorrect();
+			}
+			else {
+				updateIncorrect();
+			}
+		}	
+	}
+	
+	public class DActionListener implements ActionListener {	
+		public void actionPerformed(ActionEvent event) {
+			if (gm.getQ().getCorrectAnswer().equals("D")) {
+				updateCorrect();			
+			}
+			else {
+				updateIncorrect();
+			}
+		}	
+	}
+	
+	public class WalkListener implements ActionListener {
+		public void actionPerformed (ActionEvent event) {
+			walkAway();
+		}
+	}
+	public void updateCorrect() {
+		System.out.println("CORRECT");
+		gm.correct();
+		questionField.setText(gm.getQ().getQuestion());
+		answerAButton.setText(gm.getQ().getAnswerA());
+		answerBButton.setText(gm.getQ().getAnswerB());
+		answerCButton.setText(gm.getQ().getAnswerC());
+		answerDButton.setText(gm.getQ().getAnswerD());
+		this.f.pack();
+	}
+	
+	public void updateIncorrect() {
+		System.out.println("INCORRECT");
+		gm.incorrect();
+		System.out.println(gm.getWinnings());
+		System.exit(0);
+	}
+	
+	public void walkAway() {
+		System.out.println(gm.getWinnings());
+		System.exit(0);
+	}
+	
+	public GameManager getGameManager () {
+		return gm;
+	}
+	
+	public void setGameManager (GameManager manager) {
+		this.gm = manager;
+	}
+	
 	public void launchFrame () {
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.pack();
 		f.setVisible(true);
 	}
 	
-	public static void main (String[] args) {
-		MillionaireGUI gui = new MillionaireGUI(new Question("What is the highest U.S. mountain east of the Mississippi River?", "Mount Washington, NH", "Mount Mitchell, NC", "Mount Adams, NH", "Stone Mountain, GA", 15, "B"));
-		gui.launchFrame();
-	}
+//	public static void main (String[] args) {
+//		MillionaireGUI gui = new MillionaireGUI(gm);
+//		gui.launchFrame();
+//	}
 }
--- trunk/Millionaire/src/MillionaireMain.java
+++ trunk/Millionaire/src/MillionaireMain.java
@@ -6,5 +6,8 @@ import java.util.Scanner;
 public class MillionaireMain {
 	public static void main (String[] args) throws FileNotFoundException {
 		QuestionList qList = MillionaireFileReading.generateQuestions();
+		GameManager gm = new GameManager(qList);
+		MillionaireGUI gui = new MillionaireGUI(gm);
+		gui.launchFrame();
 	}
 }
